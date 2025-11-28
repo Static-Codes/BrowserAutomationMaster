1a6989709e4e34ad0d66ee85837aa2e95e1a2f77
Static <210101922+Static-Codes@users.noreply.github.com>
2025-11-28 11:12:08 -0500

    Removed unused import

--- src/BrowserAutomationMaster/Messaging/Clipboard.cs
+++ src/BrowserAutomationMaster/Messaging/Clipboard.cs
@@ -1,5 +1,4 @@
-﻿using BrowserAutomationMaster.Managers.Python;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
