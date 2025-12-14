66f22a6971c625abfd95e36d902df36ac1f0fc9d
Static <210101922+Static-Codes@users.noreply.github.com>
2025-12-13 23:08:59 -0500

    Added psuedocode for macOS installer

--- src/Installers/MacOS/install.sh
+++ src/Installers/MacOS/install.sh
@@ -1,1 +1,10 @@
+# Get the current version of macOS
+# Check if that version is >= 11
+# Get the current cpu architecture
+# Assign the correct download url associated with the current cpu architecture
+# Download the binary
+# If the user is on silicon, the download needs to be renamed from bamm-silicon to bamm
 
+# Give the user instructions on
+# 1. How to give the appropriate permissions to the binary ('chmod +x')
+# 2. How to bypass gatekeeper if they wish
