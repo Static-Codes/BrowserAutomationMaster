0f250adbb215e9c7d7e516e9b5d4b3532c148d2a
Static <210101922+Static-Codes@users.noreply.github.com>
2025-12-20 22:05:36 -0500

    Removed unused imports.

--- src/BrowserAutomationMaster/Managers/EditorManager.cs
+++ src/BrowserAutomationMaster/Managers/EditorManager.cs
@@ -1,10 +1,8 @@
 using System.Diagnostics;
-using static BrowserAutomationMaster.Managers.AppManager.OS.Win;
 using static BrowserAutomationMaster.Managers.AppManager.OS.Linux;
 using static BrowserAutomationMaster.Managers.ConstantManager;
 using static BrowserAutomationMaster.Managers.PlatformManager;
 using static BrowserAutomationMaster.Messaging.Errors;
-using static BrowserAutomationMaster.Messaging.Success;
 using BrowserAutomationMaster.Managers.AppManager;
 
 namespace BrowserAutomationMaster.Managers
