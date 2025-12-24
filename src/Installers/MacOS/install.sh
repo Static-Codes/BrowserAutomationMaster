f714b7b0ea6bba3bd4a6552fe670ffb086c65ad7
Static <210101922+Static-Codes@users.noreply.github.com>
2025-12-24 17:09:59 -0500

    Fixed typo "Content" -> "Contents"

--- src/Installers/MacOS/install.sh
+++ src/Installers/MacOS/install.sh
@@ -78,7 +78,7 @@ BASE_RELEASE_URL="https://github.com/Static-Codes/BrowserAutomationMaster/releas
 BUG_REPORT_LINK="https://github.com/Static-Codes/BrowserAutomationMaster/issues"
 DOWNLOAD_LOCATION="$HOME/Desktop"
 FINAL_BINARY_PATH="${DOWNLOAD_LOCATION}/BAMM.app"
-EXECUTABLE_PATH="${FINAL_BINARY_PATH}/Content/MacOS/bamm"
+EXECUTABLE_PATH="${FINAL_BINARY_PATH}/Contents/MacOS/bamm"
 GATEKEEPER_BYPASSED=1 # 1 = False | 0 = True
 MAC_TYPE="Intel" # Assuming the current mac is Intel Based since they're cheaper.
 MACOS_VERSION=$(get_macos_version)
