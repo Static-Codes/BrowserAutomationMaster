15506ab61fd9232c8f5ef34a7f57d954c7daca18
Static <210101922+Static-Codes@users.noreply.github.com>
2025-12-27 19:17:42 -0500

    Added new project to solution for tests

---
+++ src/Tests/Program.cs
@@ -0,0 +1,22 @@
+﻿using static Tests.Tests;
+using static BrowserAutomationMaster.Managers.PlatformManager;
+using static BrowserAutomationMaster.ProgramFunctions;
+
+
+// Logic from Main application around colored text.
+SetPlatform();
+await InitializeAsync(["--nohwc"]);
+
+
+var data = new Dictionary<int, (object, object)>() {
+    { 1, ( "A", "A" ) },
+    { 2, ( "B", "B" ) },
+    { 3, ( "C", "A" ) },
+    { 4, ( "A", "A" ) },
+};
+
+
+var tests = CreateTests(data);
+foreach (var test in tests) {
+    RunTest(test.Key, test.Value);
+}
---
+++ src/Tests/Tests.cs
@@ -0,0 +1,63 @@
+using static BrowserAutomationMaster.Managers.ConstantManager;
+using static BrowserAutomationMaster.Messaging.Errors;
+using static BrowserAutomationMaster.Messaging.Success;
+
+namespace Tests
+{
+    
+    public class Tests 
+    {
+
+        //  { 1, (providedValue, expectedValue) => {
+        // var res = providedValue.Equals(expectedValue);
+        // return res;
+        // }
+        // }
+
+        public static Dictionary<int, Func<bool>> CreateTests(Dictionary<int, (object, object)> data)
+        {
+            var tests = new Dictionary<int, Func<bool>>();
+
+            foreach (var element in data)
+            {
+                var (valA, valB) = element.Value;
+
+                tests.Add(element.Key, () => 
+                { 
+                    return (valA.Equals(valB)); 
+                });
+            }
+
+            return tests;
+        }
+
+        public static bool RunTest(int testNumber, Func<bool> func) 
+        {
+            var passed = false;
+            try 
+            {
+                passed = func.Invoke();
+            }
+
+            catch (Exception ex) 
+            {
+                Write
+                (
+
+                    string.Join(NLC, [
+                        $"[ERROR]: Test #{testNumber} failed.",
+                        $"[ERROR LOG]: {ex.Message}",
+                        $"[STACKTRACE]: {ex.StackTrace}"
+                    ])
+                );
+            }
+
+            if (passed) {
+                WriteSuccessMessage($"[SUCCESS]: Test #{testNumber} passed.");
+            } else {
+                Write($"[ERROR]: Test #{testNumber} failed.");
+            }
+            return passed;
+        }
+    }
+}
