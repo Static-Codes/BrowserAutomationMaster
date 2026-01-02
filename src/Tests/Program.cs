75074ca302a2a7048650195de40fd0be16d5b888
Static <210101922+Static-Codes@users.noreply.github.com>
2026-01-01 22:19:22 -0500

    Added tests

--- src/Tests/Program.cs
+++ src/Tests/Program.cs
@@ -1,8 +1,9 @@
-﻿using static Tests.Runner;
+﻿using BrowserAutomationMaster.Managers;
+using BrowserAutomationMaster.Messaging;
+using static BrowserAutomationMaster.Managers.ConstantManager;
 using static BrowserAutomationMaster.Managers.PlatformManager;
 using static BrowserAutomationMaster.ProgramFunctions;
-using BrowserAutomationMaster.Managers;
-
+using static Tests.Runner;
 
 // Logic from Main application around colored text.
 SetPlatform();
@@ -12,7 +13,19 @@ var extensionManager = new ExtensionManager("file:///home/nerdy/Desktop/xpis/lan
 // var extensionManager = new ExtensionManager("https://chromewebstore.google.com/detail/volume-booster-sound-bass/ebpckmjdefimgaenaebngljijofojncm", "chrome");
 // var extensionManager = new ExtensionManager("https://addons.mozilla.org/en-US/firefox/addon/youtube-screenshot-button/", "firefox");
 // var extensionManager = new ExtensionManager("https://addons.mozilla.org/en-US/firefox/addon/languagetool/", "firefox");
-await extensionManager.GetExtensionContents();
+
+
+
+var contents = await extensionManager.GetExtensionContents();
+
+var outputPath = await extensionManager.WriteExtensionContents(contents);
+
+if (outputPath == null) {
+    
+}
+
+
+
 Environment.Exit(0);
 
 var data = new Dictionary<int, (object, object)>() {
