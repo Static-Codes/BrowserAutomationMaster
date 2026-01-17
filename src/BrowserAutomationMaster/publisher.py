0edaf354863fda9c4b15a98b151e393d77682ea1
Static <210101922+Static-Codes@users.noreply.github.com>
2026-01-17 01:00:32 -0500

    Removed unnecessary export during publishing.

--- src/BrowserAutomationMaster/publisher.py
+++ src/BrowserAutomationMaster/publisher.py
@@ -111,9 +111,9 @@ def main():
         new_cmd = cmd
         if "dotnet rpm" in cmd or "dotnet deb" in cmd:
             if "windows" in operating_system:
-                new_cmd = f"export DOTNET_ROOT='%USERPROFILE%\\.dotnet' && set DOTNET_ROLL_FORWARD=Major && {cmd}"
+                new_cmd = f"set DOTNET_ROLL_FORWARD=Major && {cmd}"
             elif "darwin" in operating_system or "linux" in operating_system:
-                new_cmd = f"export DOTNET_ROOT='$HOME/.dotnet' && export DOTNET_ROLL_FORWARD=Major && {cmd}"
+                new_cmd = f"export DOTNET_ROLL_FORWARD=Major && {cmd}"
             else:
                 print("Unsupported operating system: {0}", operating_system)
                 exit(1)
