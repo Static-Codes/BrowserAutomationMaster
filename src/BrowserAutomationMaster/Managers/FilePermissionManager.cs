a3823ff688c1ac2bd8cf87c0fe4804f90a9dd297
Static <210101922+Static-Codes@users.noreply.github.com>
2026-02-01 17:30:12 -0500

    Added P/Invoke marshalling code generation at compile time

--- src/BrowserAutomationMaster/Managers/FilePermissionManager.cs
+++ src/BrowserAutomationMaster/Managers/FilePermissionManager.cs
@@ -5,22 +5,22 @@ namespace BrowserAutomationMaster.Managers
 {
 
     [UnsupportedOSPlatform("windows")]
-    public class UnixFilePermissionManager() 
+    public partial class UnixFilePermissionManager() 
     {
         
-        [DllImport("libc", SetLastError = true)]
+        [LibraryImport("libc", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
         // access Function Docs
         // https://pubs.opengroup.org/onlinepubs/009695399/functions/access.html
         // MacOSX.sdk is a symlink to the latest MacOSX SDK, this provides a compile time constant per rosyln's requirements for DllImport.
-        private static extern int access(string path, int amode);
+        private static partial int access(string path, int amode);
 
 
-        [DllImport("libc", SetLastError = true)]
+        [LibraryImport("libc", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
         // Direct chmod execution as opposed to spawning an additional process object.
         // If successful, chmod() returns 0.
         // If unsuccessful, chmod() returns -1
         // https://www.ibm.com/docs/en/zos/2.1.0?topic=functions-chmod-change-mode-file-directory#rtchm
-        private static extern int chmod(string pathname, UInt32 mode);
+        private static partial int chmod(string pathname, uint mode);
 
         // X_OK is a Bitmask for the libc "Execute" permission, where:
         // 1 = Permission Denied
@@ -34,7 +34,7 @@ namespace BrowserAutomationMaster.Managers
         // Docs: https://www.ibm.com/docs/en/zos/2.1.0?topic=functions-chmod-change-mode-file-directory#rtchm
         
         // Since C# interprets Octals as Decimals, 0755 must be written as it's hex representation.
-        private const UInt32 READ_WRITE_EXECUTE_MODE = 0x1ED;
+        private const uint READ_WRITE_EXECUTE_MODE = 0x1ED;
 
         public static bool HasExecutablePermissions(string filePath) {
             return access(filePath, X_OK) == 0;
