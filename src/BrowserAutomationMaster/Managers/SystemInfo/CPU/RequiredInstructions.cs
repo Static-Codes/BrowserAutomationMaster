acbc68d74ac802d1330a7f1166bf9f3e728f833c
Static <210101922+Static-Codes@users.noreply.github.com>
2026-02-16 19:53:28 -0500

    Renamed enum RequiredCPUInstruction to RequiredInstructions and moved to its own file in Managers.SystemInfo.CPU

---
+++ src/BrowserAutomationMaster/Managers/SystemInfo/CPU/RequiredInstructions.cs
@@ -0,0 +1,21 @@
+namespace BrowserAutomationMaster.Managers.SystemInfo.CPU 
+{
+    public enum RequiredInstructions 
+    { 
+        X64, 
+        AES, 
+        AVX, 
+        AVX2, 
+        BMI1, 
+        BMI2, 
+        FMA, 
+        LZCNT,
+        PCLMULQDQ, 
+        POPCNT, 
+        SSE2, 
+        SSE3, 
+        SSSE3, 
+        SSE4 
+    }
+    
+}
